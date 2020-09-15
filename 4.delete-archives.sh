7ee4cdb617c7dc6e507a63eb4c185a78032204f8
Satoshi Murakami <ssmr9dt@gmail.com>
2020-09-15 09:58:39 +0900

    Create 4.delete-archives.sh

---
+++ 4.delete-archives.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+counter=0
+
+VAULTNAME=$1
+
+for ARCHIVE in $(cat archivelist);
+do
+        echo delete $counter $ARCHIVE >> log
+        aws glacier delete-archive --account-id - --vault-name $VAULTNAME --archive-id \"$ARCHIVE\" 2>/dev/null &
+        counter=$((counter+1))
+        if [ $(ps aux | grep aws | wc -l) -gt 40 ];
+        then
+                sleep 10
+        fi
+        sleep 0.1
+done
