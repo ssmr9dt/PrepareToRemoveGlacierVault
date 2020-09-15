44784c6c6090114b6b5f7b8553668e288c1d133f
Satoshi Murakami <ssmr9dt@gmail.com>
2020-09-15 09:57:26 +0900

    Create 3.get-job-output.sh

---
+++ 3.get-job-output.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in $(cat jobid.txt);
+do
+        cd ~/
+        VAULTNAME=$(echo $i | cut -d":" -f1);
+        JOBID=$(echo $i | cut -d":" -f2);
+        echo $VAULTNAME start
+        mkdir $VAULTNAME;
+        cd $VAULTNAME;
+        aws glacier get-job-output --account-id - --vault-name $VAULTNAME --job-id $JOBID output.txt
+        split -b 10MB output.txt
+        for x in $(ls x*);
+        do
+                cat $x | sed -e "s/ArchiveId\nArchiveId/g" | sed -e "s/,/,\n/g" | grep ArchiveId | cut -d"\"" -f3 >> archivelist
+        done;
+        rm x*
+done
