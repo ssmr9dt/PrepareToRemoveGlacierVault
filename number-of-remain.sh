fcde9e73e7c29dcff472a67073991ac53b536467
Satoshi Murakami <ssmr9dt@gmail.com>
2020-09-15 10:29:51 +0900

    Update number-of-remain.sh

--- number-of-remain.sh
+++ number-of-remain.sh
@@ -2,4 +2,4 @@
 
 VAULTNAME=$1
 
-echo $(($(cat $(VAULTNAME)/archivelist | wc -l)-$(tail -n 1 log | cut -d" " -f2)))
+echo $(($(cat $VAULTNAME/archivelist | wc -l)-$(tail -n 1 $VAULTNAME/log | cut -d" " -f2)))
