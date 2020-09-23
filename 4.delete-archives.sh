16d557c942450b9b22e29e6c9a0c9adfd1040fd4
Satoshi Murakami <ssmr9dt@gmail.com>
2020-09-23 14:34:01 +0900

    I changed Pocess number.

--- 4.delete-archives.sh
+++ 4.delete-archives.sh
@@ -9,7 +9,7 @@ do
         echo delete $counter $ARCHIVE >> log
         aws glacier delete-archive --account-id - --vault-name $VAULTNAME --archive-id \"$ARCHIVE\" 2>/dev/null &
         counter=$((counter+1))
-        if [ $(ps aux | grep aws | wc -l) -gt 40 ];
+        if [ $(ps aux | grep aws | wc -l) -gt 20 ];
         then
                 sleep 10
         fi
